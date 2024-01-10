5d3a1276060fe51edd2677d27ea66bd7853b5d52
Nina Iacoponelli <nina.iacoponelli@gmail.com>
2024-01-10 11:57:47 +0100

    :arrow_up: Fix docker compose

--- projet/frontend/vite.config.ts
+++ projet/frontend/vite.config.ts
@@ -1,10 +1,14 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default ({mode}) => {
-  process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+// https://vitejs.dev/config/
+export default defineConfig(({mode}) => {
+  const env = loadEnv(mode, process.cwd(), '')
 
-  return defineConfig({
+  return {
+    define: {
+      __APP_ENV__: JSON.stringify(env.APP_ENV),
+    },
     plugins: [react()],
     server: {
       host: true,
@@ -12,5 +16,5 @@ export default ({mode}) => {
         usePolling: true
       },
     }
-  })
-}
+  }
+})
